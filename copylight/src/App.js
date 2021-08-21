eb27261452dd5939f25c5af1dca4ea588dd38380
Chowdhury Jamal <chowdhury.qaizar10@gmail.com>
2021-08-20 22:23:34 -0400

    remove unneccessar react code

--- copylight/src/App.js
+++ copylight/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import "./App.css";
 import Landing from "./components/Landing";
 import Browsing from "./components/Browsing";
 
