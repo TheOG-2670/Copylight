8794999e21da3a878f88a640bf5c8001a9e89653
Chowdhury Jamal <chowdhury.qaizar10@gmail.com>
2021-08-20 22:08:46 -0400

    add browsing and landing page

---
+++ copylight/src/components/Browsing.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Browsing = () => {
+    return (
+        <div>
+            <h1>Browsing</h1>
+            <h2>Welcome to the Browsing Page</h2>
+        </div>
+    );
+};
+
+export default Browsing;
---
+++ copylight/src/components/Landing.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Landing = () => {
+    return (
+        <div>
+            <h1>Landing</h1>
+            <h2>Welcome to the Landing Page</h2>
+        </div>
+    );
+};
+
+export default Landing;
