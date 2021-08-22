3a08fb57dfbb8ee63421f95628f9403d1269c6ba
TheOG-2670 <56968507+TheOG-2670@users.noreply.github.com>
2021-08-21 22:23:22 -0400

    Revert "Acct mgmt api"

---
+++ copylight/src/components/Pages/Payment.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Checkout from "../Checkout";
+
+const Payment = () => {
+    return (
+        <React.Fragment>
+            <Checkout/>
+        </React.Fragment>
+    );
+};
+
+export default Payment;
